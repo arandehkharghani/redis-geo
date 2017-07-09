36bbf454a27d655c31a4046aece31e16cbd16ffa
Aran Dekar <arandekar@192-168-1-5.tpgi.com.au>
2017-07-09 21:27:50 +1000

    fixed some issues with security group

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -6,7 +6,7 @@ export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=us-east-1
 export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=34.225.35.175
+export AWS_VIRTUAL_HOST=52.20.241.131
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
